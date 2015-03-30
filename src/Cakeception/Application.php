499fc67fd9ab0ed4bf9b9fc10dec20b02ba0f67c
rupaheizu <rupaheizu@hakz.co>
2015-03-30 19:27:20 +0800

    Largely refactored for flexibility

--- src/Cakeception/Application.php
+++ src/Cakeception/Application.php
@@ -2,23 +2,85 @@
 
 namespace Cakeception;
 
-use Mockery as m;
-
 class Application
 {
-	public function teardown()
+	/**
+	 * Options container
+	 *
+	 * @var array
+	 * @access protected
+	 */
+	protected $options = [];
+
+	/**
+	 * Container for Cake core libraries with assigned internal namespacing
+	 *
+	 * @var array $libraries
+	 * @access protected
+	 */
+	protected $libraries = [];
+
+	/**
+	 * Namespacing alias prefix
+	 *
+	 * @var string $aliasPrefix
+	 * @access protected
+	 */
+	protected $aliasPrefix = 'Cake\\';
+
+	public function __construct(array $options = [])
 	{
-		m::teardown();
+		$this->options = $options;
 	}
 
 	/**
-	 * Gives a certain mocked class to be used
+	 * Return the options array
+	 *
+	 * @return array
+	 */
+	public function getOptions()
+	{
+		return $this->options;
+	}
+
+	/**
+	 * Registers libraries into a self contained namespace
+	 *
+	 * @param array $libraries
+	 */
+	public function autoload(array $libraries)
+	{
+		foreach ($libraries as $namespace => $params) {
+			$this->parseParams($namespace, $params);
+		}
+	}
+
+	/**
+	 * Parse the namespace and parameters of the autoloader
 	 *
 	 * @param string $key
-	 * @return object {$key}
+	 * @param array $params
 	 */
-	public function give($key)
+	public function parseParams($namespace, $params)
 	{
-		return m::mock($key);
+
 	}
+
+	/**
+	 * Assign internal namespacing to be used by internal functions
+	 *
+	 * @param string $key
+	 * @param string $value
+	 */
+	protected function namespaceLib($key, $value)
+	{
+		$this->libraries[$this->aliasPrefix . $key] = $file;
+
+		spl_autoload_register(function($file) {
+			$file = preg_match("/^Cake\\/", $file) ? implode('/', array_shift(explode('\\', $file))) : $file;
+
+			include $file;
+		});
+	}
+
 }
